bead2b58142168e5a693530862e13eaa91fa860e
Raymond <Raymond.octavious@ocsbstudent.ca>
2017-09-27 16:38:18 -0400

    Initial

---
+++ main.py
@@ -0,0 +1,10 @@
+from scene import *
+from Splash_scene import *
+import ui
+
+
+main_view = ui.View()
+scene_view = SceneView(frame = main_view.bounds, flex = 'WH') 
+main_view.add_subview(scene_view)
+scene_view.scene = SplashScene() 
+main_view.present(hide_title_bar = True, animated = False)
